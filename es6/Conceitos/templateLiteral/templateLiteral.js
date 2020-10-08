9fde837d25aa6bdda4e159320d98d299e990bb23
Rayza Oliveira <rayza.ocr@gmail.com>
2020-10-08 13:17:27 -0300

    Estudando conceito de Template Literal

---
+++ es6/Conceitos/templateLiteral/templateLiteral.js
@@ -0,0 +1,11 @@
+// template titeral
+// incluir variaveis dentro de string 
+
+const nome = "Rayza";
+const idade = 23;
+
+// concatenar strings
+console.log('Meu nome é ' + nome + ' e tenho ' + idade + ' anos.');
+
+// com template titeral
+console.log(`Meu nome é ${nome} e tenho ${idade} anos.`);
